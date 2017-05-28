657e6958e5e18f3f64e9c5f5baa7250add1980d7
Hoon Jang <lunawyrd@gmail.com>
2017-05-28 12:26:59 +0900

    url test

--- ApiServer/ApiServer/urls.py
+++ ApiServer/ApiServer/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-
+    url(r'^test/$', views.test, name='analysis'),
+    url(r'^test/test/$', views.test2, name='analysis'),
 ]
--- ApiServer/ApiServer/views.py
+++ ApiServer/ApiServer/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def test(request):
+    return HttpResponse("test")
+
+
+def test2(request):
+    return HttpResponse("test2")
